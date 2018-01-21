6a054c2253377eb706b486937bddd83d15a57bca
Phil Coltrane <phil.m.coltrane@gmail.com>
2018-01-21 13:28:55 -0500

    `npm run build` runs webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['env']
             }
           }
         }
